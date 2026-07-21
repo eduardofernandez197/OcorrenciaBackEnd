bf88df91fc114375cdc83841f5474eac919f7fce
eduardofernandez197 <eduardofefe1970@gmail.com>
2026-07-21 07:51:13 -0300

    Ajuste da classe ValidarFormatoFoto, e FotoStorage Service

--- src/main/java/com/coruja/ocorrencias/Service/validation/validaFormatoFoto.java
+++ src/main/java/com/coruja/ocorrencias/Service/validation/validaFormatoFoto.java
@@ -1,13 +1,14 @@
 package com.coruja.ocorrencias.service.validation;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.coruja.ocorrencias.dto.request.ObservacoesRequestDTO;
-import com.coruja.ocorrencias.dto.request.OcorrenciaRequestDTO;
+
 import com.coruja.ocorrencias.exception.BusinessException;
 import com.coruja.ocorrencias.service.arquivos.SalvaFotoInterface;
 @Component
@@ -15,9 +16,10 @@ import com.coruja.ocorrencias.service.arquivos.SalvaFotoInterface;
 public class validaFormatoFoto implements SalvaFotoInterface {
     //Metodo que valida se a foto nao veio vazia e se veio no formato certo
 
-    public ObservacoesRequestDTO salvarFoto(ObservacoesRequestDTO dto) {
+    public List<String> salvarFoto(ObservacoesRequestDTO dto) {
 
         List<MultipartFile> foto = dto.getImagens();
+        List<String> nomesFotos = new ArrayList<>();
 
         for (MultipartFile fotos : foto) {
             String nome = fotos.getOriginalFilename();
@@ -29,7 +31,7 @@ public class validaFormatoFoto implements SalvaFotoInterface {
                 throw new BusinessException("Apenas arquivos JPG e PNG sao permitidos");
             }
         }
-        return dto;
-
+      
+        
     }
 }
